90e75fa7986c4815f260a87daf8a474d05551405
Kenni Bawden <kenni.bawden@gmail.com>
2016-03-28 23:08:15 +1100

    Fixed admin user auth privileges

--- swipe-local-history/backend/auth.sql
+++ swipe-local-history/backend/auth.sql
@@ -85,5 +85,6 @@ grant admin to authenticator;
 
 grant usage on schema public, basic_auth to admin;
 grant usage on schema public, basic_auth to postgres;
-grant select on basic_auth.users to postgres;
-grant all privileges on schema public, hnm to admin;
+grant usage on basic_auth.users to postgres;
+grant usage on schema public, hnm to admin;
+grant all privileges on story, story_id_seq, link, link_id_seq to admin;
