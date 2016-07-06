00b870b4ba844a435888e6e23cf5ae048ce2ddec
Peter W <techieshark@gmail.com>
2016-07-06 20:46:46 +1000

    add rate limiting when geocoding
    needed to slow down geocoding b/c we have 10items/sec limit from Google geocoder
    Also adds colorful error messages

--- etl/geocoder.js
+++ etl/geocoder.js
@@ -4,23 +4,32 @@ module.exports = { geocode: geocode }
 
 const NodeGeocoder = require("node-geocoder");
 const geocoder = NodeGeocoder();
+const chalk = require("chalk");
+// TODO: use `winston` for logging to file / console / with colors
 
 function geocode (heritageItems, callback) {
 
 
-    const geocodePromises = heritageItems.map((item) => new Promise (function (resolve, reject) {
+    const geocodePromises = heritageItems.map((item, index) => new Promise (function (resolve, reject) {
 
-        geocoder.geocode(item.address, function setLocation (err, res) {
-            if (err) {
-                reject(`geocoding address "${item.address}" failed: ${err}`)
-            }
+        // delay each API query long enough so we don't hit Google's 10 queries / second limit
+        // a delay > 100ms between queries should do the trick (10 queries * 101ms gives  >1 second / 10 queries)
+        setTimeout (() => {
+            geocoder.geocode(item.address, function setLocation (err, res) {
+                if (err) {
+                    reject(`geocoding address "${item.address}" failed: ${err}`)
+                } else {
+                    item.location = {}
+                    item.location.latitude = res[0].latitude
+                    item.location.longitude = res[0].longitude
 
-            item.location = {}
-            item.location.latitude = res[0].latitude
-            item.location.longitude = res[0].longitude
+                    resolve(item.location)
+                }
 
-            resolve(item.location)
-        })
+
+            })
+        }, index * 150)
+
     }))
 
     Promise.all(geocodePromises).then(values => {
@@ -29,9 +38,14 @@ function geocode (heritageItems, callback) {
         callback(null, heritageItems) // should all be geocoded now
     })
     .catch(reason => {
-        console.log("Geocoding failed: " + reason)
-        console.log(heritageItems)
-        console.log(geocodePromises)
+        // uncomment this if you need it for debugging or something.
+        // console.error(chalk.red("----------------------------       ----------------------------"))
+        // console.error(chalk.red("---------------------------- ERROR ----------------------------"))
+        // console.error(chalk.red("----------------------------       ----------------------------"))
+        // console.error(heritageItems)
+        // console.error(geocodePromises)
+        console.error(chalk.red("Geocoder: " + reason))
+        process.exit(1)
     })
 }
 
