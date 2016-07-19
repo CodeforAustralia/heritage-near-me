327da1512a976e290c02eafad4a0afc5f83c506e
Peter W <techieshark@gmail.com>
2016-07-19 16:48:13 +1000

    Data.elm: add support for getting story details *with distance*, plus docs

--- prototype/src/Data.elm
+++ prototype/src/Data.elm
@@ -1,4 +1,20 @@
-module Data (requestNearbyStories, requestStories, requestStory, viewStory, favouriteStory, getItem, updateStory) where
+module Data (requestNearbyStories, requestStories, requestNearbyStory, requestStory, viewStory, favouriteStory, getItem, updateStory) where
+
+{-| The Data module has functions to get and send data to the server.
+
+# Requesting from server
+
+@docs requestStories, requestNearbyStories, requestStory, requestNearbyStory
+
+# Measuring views / favorites / passes for analytical purposes.
+
+@docs viewStory, favouriteStory
+
+# Get or update single items
+
+@docs getItem, updateStory
+
+-}
 
 import Json.Decode as Json exposing ((:=), andThen)
 import Json.Decode.Extra exposing ((|:))
@@ -22,7 +38,15 @@ import Native.TimeTask
 getItem : StoryId -> AppModel -> RemoteData Story
 getItem id app = Remote.DataStore.get id app.items
 
-{-| A function to update a story in the data store -}
+
+{-| A function to update a story in the data store.
+
+This function considers both `newStory` and `oldStory` (and `oldStory` may
+or may not exist in the data store, hence it is a `Maybe` type), and updates
+our internal collection of stories. While doing so, it ensures that we
+don't accidentally load a DiscoveryStory (a subset of FullStory) in place of
+a FullStory.
+-}
 updateStory : RemoteData Story -> Maybe (RemoteData Story) -> Maybe (RemoteData Story)
 updateStory newStory oldStory = case (Remote.Data.get newStory, oldStory `Maybe.andThen` Remote.Data.get) of
             -- Only load full stories over the top of discover stories and not the other way around
@@ -36,19 +60,85 @@ updateStory newStory oldStory = case (Remote.Data.get newStory, oldStory `Maybe.
 apiUrl : String -> String
 apiUrl subUrl = Http.url ("api/"++subUrl) []
 
+
+{-| Returns an Http.Request matching our API, to be used by operations like Http.send.
+
+`apiMethod` is currently an SQL function name, like "story_discover_by_location"
+ See heritage-near-me.sql for the SQL that sets that up on the server.
+`pos` is the user's lat/long, because requestNearbyStories and requestNearbyStory
+ need it and are the only things using this function currently.
+
+PostgREST only allows query parameters for GET requests (like we use when hitting a VIEW),
+and not for POST requests (like we use when hitting the story_details_by_location RPC),
+so if we want to filter story details by storyId we can do that by supplying it to this
+function instead of sending it as a query parameter.
+
+Example for requesting story details:
+```
+    apiHttpPostRequest "story_details_by_location" {lat = "34.123", lng = "-150.123"} (Just 21)
+```
+-}
+apiHttpPostRequest : String -> LatLng -> Maybe StoryId -> Http.Request
+apiHttpPostRequest apiMethod pos maybeStory =
+    let
+        --id = Debug.log "id: " (idToString storyId)
+        storyPart = case maybeStory of
+            Just (StoryId id) -> ", \"story_of_interest\": \""++(toString id)++"\""
+            Nothing -> ""
+    in
+        { verb = "POST"
+        , headers = [("Content-Type", "application/json")]
+        , url = apiUrl "rpc/" ++ apiMethod
+        , body = Http.string <| "{\"lat\": \""++pos.lat++"\", \"lng\": \""++pos.lng++"\""++storyPart++"}"
+    }
+
+{-| The version of apiHttpPostRequest that has a default final parameter of Nothing.
+
+Useful when you don't need to pass story in the POST body.
+ -}
+apiHttpPostRequest' : String -> LatLng -> Http.Request
+apiHttpPostRequest' apiMethod pos = apiHttpPostRequest apiMethod pos Nothing
+
+
 {-| Http Request for Discovery stories with distances from current location -}
 requestNearbyStories : LatLng -> Task Http.Error (List Story)
-requestNearbyStories pos = Http.send Http.defaultSettings
-    { verb = "POST"
-    , headers = [("Content-Type", "application/json")]
-    , url = apiUrl "rpc/story_discover_by_location"
-    , body = Http.string <| "{\"lat\": \""++pos.lat++"\", \"lng\": \""++pos.lng++"\"}"
-    }
+requestNearbyStories pos =
+    Http.send
+        Http.defaultSettings
+        (apiHttpPostRequest' "story_discover_by_location" pos)
     |> Http.fromJson discoverStories
+
 
 {-| Http Request for Discovery stories -}
 requestStories : Task Http.Error (List Story)
 requestStories = Http.get discoverStories <| apiUrl "story_discover"
+
+
+storyNotFoundError : Task Http.Error a
+storyNotFoundError =
+    Task.fail (Http.BadResponse 404 "Story with given id was not found")
+
+
+{-| Http Request for a given story Id (our LatLng given so server includes distance from us) -}
+requestNearbyStory: LatLng -> StoryId -> Task Http.Error Story
+requestNearbyStory pos storyId =
+     (Task.map List.head -- apply .head to the list of full stories, so we just get the first
+        <| Http.fromJson -- : Decoder (List Story) -> Task RawError Response -> Task Error (List Story)
+            fullStoriesDecoder -- : Decoder (List Story)
+            (Http.send -- : Settings -> Request -> Task RawError Response
+                Http.defaultSettings
+                (apiHttpPostRequest "story_details_by_location" pos (Just storyId))
+            )
+    ) -- : Task Http.Error (Maybe Story)
+    `Task.andThen` -- after mapping head onto .fromJson, do this:
+        (\firstStory -> -- Maybe Story
+            Maybe.withDefault
+                (Task.fail <| Http.BadResponse 404 ("Story with ID of '" ++
+                    Story.storyIdToStr storyId ++ "' was not found"))
+                <| Maybe.map Task.succeed firstStory
+        )
+
+
 
 {-| Http Request for a given Story Id -}
 requestStory : StoryId -> Task Http.Error Story
@@ -56,7 +146,7 @@ requestStory storyId = let
         (StoryId id) = storyId
     in
         (Task.map List.head
-        <| Http.get fullStories
+        <| Http.get fullStoriesDecoder
         <| Http.url (apiUrl "story_details") [("id", "eq." ++ toString id)])
         `Task.andThen`
             (\storyId -> Maybe.withDefault
@@ -119,8 +209,8 @@ discoverStory = Json.object6
     ("sites" := Json.list siteDecoder)
 
 {- List of Stories JSON Decoder -}
-fullStories : Json.Decoder (List Story)
-fullStories = Json.list fullStoryDecoder
+fullStoriesDecoder: Json.Decoder (List Story)
+fullStoriesDecoder = Json.list fullStoryDecoder
 
 {-| Story JSON decoder -}
 fullStoryDecoder : Json.Decoder Story
