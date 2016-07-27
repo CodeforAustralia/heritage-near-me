c0b1975c7cf35921c5b9de5b8863d51d5078a42c
Peter W <techieshark@gmail.com>
2016-07-27 15:40:24 +1000

    routing code so we can navigate from story body to intro

--- prototype/src/Route.elm
+++ prototype/src/Route.elm
@@ -1,5 +1,7 @@
 module Route (action, url) where
 
+import Story exposing (storyIdToStr)
+
 import String
 import Result
 import RouteHash exposing (HashUpdate)
@@ -11,7 +13,18 @@ action : List String -> List AppAction
 action url = case url of
     "discover"::_ -> [Discover]
     "favourites"::_ -> [ViewFavourites]
-    "story"::storyId::_ -> [View <| StoryId <| Maybe.withDefault -1 <| Result.toMaybe <| String.toInt storyId]
+    "story"::storyId::storyScreen::_ ->
+        let
+            id = parseStoryId storyId
+            screen = parseStoryScreen storyScreen
+        in
+            [View id screen]
+    "story"::storyId::_ ->
+        let
+            id = parseStoryId storyId
+            screen = screen1
+        in
+            [View id screen]
     _ -> Debug.log "converted url into default action: " [Discover]
 
 {-| Turn the difference between two app states into a browser URL -}
@@ -20,10 +33,30 @@ url old new = if old.location /= new.location then
         Just <| case new.location of
             Discovering       -> RouteHash.set ["discover"]
             ViewingFavourites -> RouteHash.set ["favourites"]
-            Viewing storyId _ _ -> RouteHash.set ["story", urliseStory storyId]
+            Viewing storyId _ storyScreen -> RouteHash.set ["story", storyIdToStr storyId, urliseStoryScreen storyScreen]
     else
         Nothing
 
-{- Turn a story's id into a URL -}
-urliseStory : StoryId -> String
-urliseStory (StoryId id) = toString id
+
+{-| Parse what might be the story ID -}
+parseStoryId : String -> StoryId
+parseStoryId storyId =
+    StoryId <| Maybe.withDefault -1 <| Result.toMaybe <| String.toInt storyId
+
+{-| Parse what might be the story screen -}
+parseStoryScreen : String -> StoryScreen
+parseStoryScreen storyScreen =
+    case storyScreen of
+        "body" ->
+            Body
+        _ -> Intro
+
+
+{-| Turn a story screen into a string to be used in the URL -}
+urliseStoryScreen : StoryScreen -> String
+urliseStoryScreen screen =
+    case screen of
+        Intro ->
+            ""
+        Body ->
+            "body"
